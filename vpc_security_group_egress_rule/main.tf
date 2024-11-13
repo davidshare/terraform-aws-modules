2471ea59e49c95e1556ecd83f53471eee436d05b
David Essien <davidessienshare@gmail.com>
2024-11-13 20:36:34 +0100

    update ingress and egress rule modules

--- vpc_security_group_egress_rule/main.tf
+++ vpc_security_group_egress_rule/main.tf
@@ -1,13 +1,14 @@
 resource "aws_vpc_security_group_egress_rule" "this" {
-  security_group_id          = var.security_group_id
-  cidr_ipv4                  = var.cidr_ipv4
-  cidr_ipv6                  = var.cidr_ipv6
-  from_port                  = var.from_port
-  to_port                    = var.to_port
-  ip_protocol                = var.ip_protocol
-  prefix_list_id             = var.prefix_list_id
-  referenced_security_group_id = var.referenced_security_group_id
-  description                = var.description
+  security_group_id = var.security_group_id
+  from_port         = var.from_port
+  to_port           = var.to_port
+  ip_protocol       = var.ip_protocol
+  description       = try(var.description, null)
 
-  tags = var.tags
+  cidr_ipv4                    = var.cidr_ipv4 != "" ? var.cidr_ipv4 : null
+  cidr_ipv6                    = var.cidr_ipv6 != "" ? var.cidr_ipv6 : null
+  prefix_list_id               = var.prefix_list_id != "" ? var.prefix_list_id : null
+  referenced_security_group_id = var.referenced_security_group_id != "" ? var.referenced_security_group_id : null
+
+  tags              = var.tags
 }
