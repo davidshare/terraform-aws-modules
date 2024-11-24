c9dc2fcbcf29c4a9ce51380cd9aefbc407342de3
David Essien <davidessienshare@gmail.com>
2024-11-24 22:21:41 +0100

    delete other modules

--- sns_topic/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "The ARN of the SNS topic."
-  value       = aws_sns_topic.this.arn
-}
-
-output "name" {
-  description = "The name of the SNS topic."
-  value       = aws_sns_topic.this.name
-}
