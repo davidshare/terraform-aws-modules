3bc1015bc582c5a0aa69517340fc3fdf17d9d8f5
David Essien <davidessienshare@gmail.com>
2024-11-24 23:47:35 +0100

    delete other modules

--- cloudwatch_metric_alarm/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "The ARN of the CloudWatch Metric Alarm."
-  value       = aws_cloudwatch_metric_alarm.this.arn
-}
-
-output "id" {
-  description = "The ID of the CloudWatch Metric Alarm."
-  value       = aws_cloudwatch_metric_alarm.this.id
-}
