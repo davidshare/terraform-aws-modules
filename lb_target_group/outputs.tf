6efb524092de32fcd00723bb62e206474e3425ad
David Essien <davidessienshare@gmail.com>
2024-11-24 21:53:19 +0100

    delete other modules

--- lb_target_group/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "The ARN of the target group"
-  value       = aws_lb_target_group.this.arn
-}
-
-output "name" {
-  description = "The name of the target group"
-  value       = aws_lb_target_group.this.name
-}
