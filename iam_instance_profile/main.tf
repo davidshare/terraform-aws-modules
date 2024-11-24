764d96151a38320737f9e0e6b81f84358ebd356e
David Essien <davidessienshare@gmail.com>
2024-11-24 21:27:07 +0100

    delete other modules

--- iam_instance_profile/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_iam_instance_profile" "instance_profile" {
-  name        = var.name
-  role        = var.role
-  path        = var.path
-  tags        = var.tags
-  name_prefix = var.name_prefix
-}
--- target_group/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "ARN of the target group"
-  value       = aws_lb_target_group.this.arn
-}
-
-output "name" {
-  description = "Name of the target group"
-  value       = aws_lb_target_group.this.name
-}
