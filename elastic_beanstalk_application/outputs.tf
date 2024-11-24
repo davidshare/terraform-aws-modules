5c0c0a1be1281c57b9a4a30db8c0bd17009552a0
David Essien <davidessienshare@gmail.com>
2024-11-24 23:13:23 +0100

    delete other modules

--- elastic_beanstalk_application/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  description = "The name of the Elastic Beanstalk Application"
-  value       = aws_elastic_beanstalk_application.this.name
-}
-
-output "arn" {
-  description = "The ARN of the Elastic Beanstalk Application"
-  value       = aws_elastic_beanstalk_application.this.arn
-}
--- iam_role_policy/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_iam_role_policy" "this" {
-  name        = var.name
-  name_prefix = var.name_prefix
-  role        = var.role
-  policy      = var.policy
-}
