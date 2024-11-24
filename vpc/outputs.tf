3f84f2a348c8c73ccb7beb4a40fd23247feefd37
David Essien <davidessienshare@gmail.com>
2024-11-24 10:18:36 +0100

    update vpc outputs

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,64 +1,64 @@
-output "vpc_id" {
+output "id" {
   description = "The ID of the VPC"
   value       = aws_vpc.this.id
 }
 
-output "vpc_arn" {
+output "arn" {
   description = "The ARN of the VPC"
   value       = aws_vpc.this.arn
 }
 
-output "vpc_cidr_block" {
+output "cidr_block" {
   description = "The CIDR block of the VPC"
   value       = aws_vpc.this.cidr_block
 }
 
-output "vpc_instance_tenancy" {
+output "instance_tenancy" {
   description = "Tenancy of instances spin up within VPC"
   value       = aws_vpc.this.instance_tenancy
 }
 
-output "vpc_enable_dns_support" {
+output "enable_dns_support" {
   description = "Whether or not the VPC has DNS support"
   value       = aws_vpc.this.enable_dns_support
 }
 
-output "vpc_enable_dns_hostnames" {
+output "enable_dns_hostnames" {
   description = "Whether or not the VPC has DNS hostname support"
   value       = aws_vpc.this.enable_dns_hostnames
 }
 
-output "vpc_main_route_table_id" {
+output "main_route_table_id" {
   description = "The ID of the main route table associated with this VPC"
   value       = aws_vpc.this.main_route_table_id
 }
 
-output "vpc_default_network_acl_id" {
+output "default_network_acl_id" {
   description = "The ID of the network ACL created by default on VPC creation"
   value       = aws_vpc.this.default_network_acl_id
 }
 
-output "vpc_default_security_group_id" {
+output "default_security_group_id" {
   description = "The ID of the security group created by default on VPC creation"
   value       = aws_vpc.this.default_security_group_id
 }
 
-output "vpc_default_route_table_id" {
+output "default_route_table_id" {
   description = "The ID of the route table created by default on VPC creation"
   value       = aws_vpc.this.default_route_table_id
 }
 
-output "vpc_ipv6_association_id" {
+output "ipv6_association_id" {
   description = "The association ID for the IPv6 CIDR block"
   value       = aws_vpc.this.ipv6_association_id
 }
 
-output "vpc_ipv6_cidr_block" {
+output "ipv6_cidr_block" {
   description = "The IPv6 CIDR block"
   value       = aws_vpc.this.ipv6_cidr_block
 }
 
-output "vpc_owner_id" {
+output "owner_id" {
   description = "The ID of the AWS account that owns the VPC"
   value       = aws_vpc.this.owner_id
 }
