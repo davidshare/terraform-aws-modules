480517d13a27128db63529056b1d695831fc0016
David Essien <davidessienshare@gmail.com>
2024-11-24 18:22:48 +0100

    update internet gateway outputs and README

--- internet_gateway/outputs.tf
+++ internet_gateway/outputs.tf
@@ -1,14 +1,14 @@
-output "internet_gateway_id" {
+output "id" {
   description = "The ID of the Internet Gateway"
   value       = aws_internet_gateway.this.id
 }
 
-output "internet_gateway_arn" {
+output "arn" {
   description = "The ARN of the Internet Gateway"
   value       = aws_internet_gateway.this.arn
 }
 
-output "internet_gateway_owner_id" {
+output "owner_id" {
   description = "The ID of the AWS account that owns the Internet Gateway"
   value       = aws_internet_gateway.this.owner_id
 }
