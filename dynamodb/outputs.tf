f7846172ba5517912e708fcdf3e61526b62efc87
David Essien <davidessienshare@gmail.com>
2024-11-24 17:40:57 +0100

    update dynamodb outputs and readme

--- dynamodb/outputs.tf
+++ dynamodb/outputs.tf
@@ -1,14 +1,14 @@
-output "table_arn" {
+output "arn" {
   description = "ARN of the DynamoDB table"
   value       = aws_dynamodb_table.this.arn
 }
 
-output "table_id" {
+output "id" {
   description = "ID of the DynamoDB table"
   value       = aws_dynamodb_table.this.id
 }
 
-output "table_stream_arn" {
+output "stream_arn" {
   description = "The ARN of the Table Stream. Only available when stream_enabled = true"
   value       = var.stream_enabled ? aws_dynamodb_table.this.stream_arn : null
 }
