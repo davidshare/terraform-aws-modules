1a4c7dc417a819b37308d2c43c23370b02644b29
David Essien <davidessienshare@gmail.com>
2024-11-24 17:52:54 +0100

    update eks readme and outputs

--- eks/output.tf
+++ eks/output.tf
@@ -1,30 +1,30 @@
 
-output "cluster_id" {
+output "id" {
   description = "The name/id of the EKS cluster"
   value       = aws_eks_cluster.this.id
 }
 
-output "cluster_arn" {
+output "arn" {
   description = "The Amazon Resource Name (ARN) of the cluster"
   value       = aws_eks_cluster.this.arn
 }
 
-output "cluster_certificate_authority_data" {
+output "certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with the cluster"
   value       = aws_eks_cluster.this.certificate_authority[0].data
 }
 
-output "cluster_endpoint" {
+output "endpoint" {
   description = "Endpoint for your Kubernetes API server"
   value       = aws_eks_cluster.this.endpoint
 }
 
-output "cluster_version" {
+output "version" {
   description = "The Kubernetes server version of the cluster"
   value       = aws_eks_cluster.this.version
 }
 
-output "cluster_platform_version" {
+output "platform_version" {
   description = "Platform version for the cluster"
   value       = aws_eks_cluster.this.platform_version
 }
