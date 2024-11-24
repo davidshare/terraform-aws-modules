007fb140ffd9b06e34a6f443ce39fa0af9b9bb20
David Essien <davidessienshare@gmail.com>
2024-11-24 17:22:18 +0100

    update cloudwatch metric alarm output and readme

--- cloudwatch_metric_alarm/outputs.tf
+++ cloudwatch_metric_alarm/outputs.tf
@@ -1,9 +1,9 @@
-output "alarm_arn" {
+output "arn" {
   description = "The ARN of the CloudWatch Metric Alarm."
   value       = aws_cloudwatch_metric_alarm.this.arn
 }
 
-output "alarm_id" {
+output "id" {
   description = "The ID of the CloudWatch Metric Alarm."
   value       = aws_cloudwatch_metric_alarm.this.id
 }
