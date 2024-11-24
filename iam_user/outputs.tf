d64e6fc52a60c627b10fa33aa23de1adfafb111a
David Essien <davidessienshare@gmail.com>
2024-11-24 18:21:50 +0100

    update iam user README and outputs

--- iam_user/outputs.tf
+++ iam_user/outputs.tf
@@ -1,14 +1,14 @@
-output "iam_user_name" {
+output "name" {
   description = "The user's name"
   value       = aws_iam_user.this.name
 }
 
-output "iam_user_arn" {
+output "arn" {
   description = "The ARN assigned by AWS for this user"
   value       = aws_iam_user.this.arn
 }
 
-output "iam_user_unique_id" {
+output "unique_id" {
   description = "The unique ID assigned by AWS"
   value       = aws_iam_user.this.unique_id
 }
