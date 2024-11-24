3dcedbfc1ab46964fb97be99633c464de0bdaf3d
David Essien <davidessienshare@gmail.com>
2024-11-24 17:48:16 +0100

    update ecr outputs and readme

--- ecr/outputs.tf
+++ ecr/outputs.tf
@@ -1,14 +1,14 @@
-output "repository_url" {
+output "url" {
   description = "The URL of the repository"
   value       = aws_ecr_repository.this.repository_url
 }
 
-output "repository_arn" {
+output "arn" {
   description = "The ARN of the repository"
   value       = aws_ecr_repository.this.arn
 }
 
-output "repository_registry_id" {
+output "registry_id" {
   description = "The registry ID where the repository was created"
   value       = aws_ecr_repository.this.registry_id
 }
