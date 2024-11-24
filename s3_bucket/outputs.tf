8c0afe186c68d1d6dbe1898731c22ea650fbac3a
David Essien <davidessienshare@gmail.com>
2024-11-24 18:38:15 +0100

    update s3_bucket outputs and readme

--- s3_bucket/outputs.tf
+++ s3_bucket/outputs.tf
@@ -1,19 +1,19 @@
-output "s3_bucket_id" {
+output "id" {
   description = "The name of the bucket"
   value       = aws_s3_bucket.this.id
 }
 
-output "s3_bucket_arn" {
+output "arn" {
   description = "The ARN of the bucket"
   value       = aws_s3_bucket.this.arn
 }
 
-output "s3_bucket_domain_name" {
+output "domain_name" {
   description = "The bucket domain name"
   value       = aws_s3_bucket.this.bucket_domain_name
 }
 
-output "s3_bucket_regional_domain_name" {
+output "regional_domain_name" {
   description = "The bucket region-specific domain name"
   value       = aws_s3_bucket.this.bucket_regional_domain_name
 }
