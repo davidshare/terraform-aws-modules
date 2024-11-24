772317e88d5067cb72e44736c47f1df03c7fb58b
David Essien <davidessienshare@gmail.com>
2024-11-24 18:48:35 +0100

    update sqs_queue readme and outputs

--- sqs_queue/outputs.tf
+++ sqs_queue/outputs.tf
@@ -1,14 +1,14 @@
-output "sqs_queue_id" {
+output "id" {
   description = "The URL for the created Amazon SQS queue"
   value       = aws_sqs_queue.this.id
 }
 
-output "sqs_queue_arn" {
+output "arn" {
   description = "The ARN of the SQS queue"
   value       = aws_sqs_queue.this.arn
 }
 
-output "sqs_queue_name" {
+output "name" {
   description = "The name of the SQS queue"
   value       = aws_sqs_queue.this.name
 }
