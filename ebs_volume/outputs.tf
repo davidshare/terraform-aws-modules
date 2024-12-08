16c8938d7813597dc3498be683acd9f3255f39ca
David Essien <davidessienshare@gmail.com>
2024-12-08 08:46:53 +0100

    delete other modules apart from ec2

--- ebs_volume/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "The volume ID"
-  value       = aws_ebs_volume.this.id
-}
-
-output "arn" {
-  description = "The volume ARN"
-  value       = aws_ebs_volume.this.arn
-}
