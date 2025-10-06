c66c1c26c1983adbb14ae6eff956df7c220fab73
David Essien <davidessienshare@gmail.com>
2025-10-06 15:20:25 +0100

    add s3_bucket_notification module

---
+++ s3_bucket_notification/main.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket_notification" "this" {
+  bucket = var.bucket
+
+  dynamic "lambda_function" {
+    for_each = var.lambda_functions
+    content {
+      id                  = lookup(lambda_function.value, "id", null)
+      lambda_function_arn = lambda_function.value.lambda_function_arn
+      events              = lambda_function.value.events
+      filter_prefix       = lookup(lambda_function.value, "filter_prefix", null)
+      filter_suffix       = lookup(lambda_function.value, "filter_suffix", null)
+    }
+  }
+
+  dynamic "topic" {
+    for_each = var.topics
+    content {
+      id         = lookup(topic.value, "id", null)
+      topic_arn  = topic.value.topic_arn
+      events     = topic.value.events
+      filter_prefix = lookup(topic.value, "filter_prefix", null)
+      filter_suffix = lookup(topic.value, "filter_suffix", null)
+    }
+  }
+
+  dynamic "queue" {
+    for_each = var.queues
+    content {
+      id         = lookup(queue.value, "id", null)
+      queue_arn  = queue.value.queue_arn
+      events     = queue.value.events
+      filter_prefix = lookup(queue.value, "filter_prefix", null)
+      filter_suffix = lookup(queue.value, "filter_suffix", null)
+    }
+  }
+}
