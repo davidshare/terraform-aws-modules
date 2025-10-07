89a5e5fcb9fda827f6da2edef36d8b263a7a2e7d
David Essien <davidessienshare@gmail.com>
2025-10-07 15:13:06 +0100

    remove other modules

--- s3_bucket_policy/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "The ID of the bucket policy (same as bucket name)"
-  value       = aws_s3_bucket_policy.this.id
-}
-
-output "policy" {
-  description = "The applied bucket policy"
-  value       = aws_s3_bucket_policy.this.policy
-}
