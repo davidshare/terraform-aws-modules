08667013c66219d73f0259670f3116e78aa7e704
David Essien <davidessienshare@gmail.com>
2025-10-08 19:42:36 +0100

    add s3_bucket_versioning module

---
+++ s3_bucket_versioning/main.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = var.bucket
+  expected_bucket_owner = var.expected_bucket_owner
+  mfa                   = var.mfa
+
+  versioning_configuration {
+    status     = var.versioning_configuration.status
+    mfa_delete = var.versioning_configuration.mfa_delete
+  }
+}
