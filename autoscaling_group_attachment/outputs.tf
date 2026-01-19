be8d6e765123ad59b0d73a4000bb5c1a035122f7
David Essien <davidessienshare@gmail.com>
2026-01-19 11:52:09 +0100

    add ecs cluster module

--- autoscaling_group_attachment/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "id" {
-  description = "The ID of the Auto Scaling attachment."
-  value       = aws_autoscaling_attachment.this.id
-}
--- iam_role_policy_attachment/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "role" {
-  description = "The name of the IAM role to which the managed policy will be attached."
-  type        = string
-}
-
-variable "policy_arn" {
-  description = "The ARN of the IAM managed policy to attach to the role."
-  type        = string
-}
